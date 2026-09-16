1deae5e9ab1de82413ce9ebf749a4930138930f4
hopp1t <egoregor3310@gmail.com>
2026-09-16 19:38:47 +0600

    feat: step 2 form matrix A

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ import numpy as np
 class MatrixLab:
     def __init__(self):
         self.my_array = None
+        self.A = None
 
-    def step_1_create_array(self):
+    def create_array(self):
         print("\n--- Шаг 1. Создание массива ---")  
         self.my_array = np.arange(10, 70, 2)
         print("Массив my_array:")
         print(self.my_array)
 
+    def form_matrix_A(self):
+        print("\n--- Шаг 2. Формирование матрицы A ---")
+        self.A = self.my_array.reshape(6, 5).T
+        print(f"Размерность матрицы A: {self.A.shape}")
+        print("Матрица A:")
+        print(self.A)
+
     def run(self):
-        self.step_1_create_array()
+        self.create_array()
+        self.form_matrix_A()
 
 if __name__ == "__main__":
     lab = MatrixLab()
